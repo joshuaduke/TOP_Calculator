0428c1aea5bd84de6af8b0f648194cf2c5be41e8
Joshua Duke <dukejoshuae@gmail.com>
2021-10-28 16:36:08 -0400

    display label working

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let inputlabel = document.querySelector('.inputContainer label');
+let clearBtn = document.querySelector('.clear');
+let btnOptions = document.querySelectorAll('.myNumBtn');
+
+clearBtn.addEventListener('click', clearLabel)
+
 function add(num1, num2){
     console.log(num1 + num2)
     return num1 + num2;
@@ -35,4 +41,45 @@ function operate(num1, num2, operator){
     }
 }
 
-operate(2,4,'/');
+function clear(){
+
+}
+
+function selectOperator(){
+    let operators = document.querySelectorAll('.myOperator');
+
+}
+
+btnOptions.forEach(btn => {
+    btn.addEventListener('click', changeDisplay)
+});
+
+function changeDisplay(e){
+    // 
+    if(inputlabel.innerText == '0'){
+        inputlabel.innerText = '' 
+        console.log(e.target.innerText)
+        if(inputlabel.innerText.length <= 9){
+            inputlabel.innerText += e.target.innerText
+        }
+    } else {
+        console.log(e.target.innerText)
+        if(inputlabel.innerText.length <= 9){
+            inputlabel.innerText += e.target.innerText
+        }
+    }
+    
+}
+
+
+
+function clearLabel(){
+    inputlabel.innerText = 0;
+}
+
+//user enters first number 
+// user selects an operator
+// first number is stored
+// user selects a second number
+//if user selects another operator, 
+//display result of the first operation 
